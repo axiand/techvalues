0b240a10525744f483ebd4fcd244e5d0256df06e
Axi <98420477+axiand@users.noreply.github.com>
2022-11-21 22:08:50 +0100

    MIlestone: 30 questions

--- questions.js
+++ questions.js
@@ -1,8 +1,140 @@
 questions = [
     {
-        "question": "Sample text",
+        "question": "I prefer Linux over Windows.",
         "effect": {
             "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Adobe's business model is predatory.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I build open-source software myself before using it.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": -5,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I enjoy planning out database schemas.",
+        "effect": {
+            "fend": -10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 5
+        }
+    },
+    {
+        "question": "I prefer to use alternative (not modded) clients for apps whenever possible.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I am familiar with front-end JavaScript frameworks.",
+        "effect": {
+            "fend": 10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use Node.js for my app's backend.",
+        "effect": {
+            "fend": -10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 10,
+            "conv": 0,
+            "popu": 10,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use ASP.NET for my app's backend.",
+        "effect": {
+            "fend": -10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm familiar with niche operating systems e.g. Haiku",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": -10,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use GIMP over other image editors.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": -5,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use legacy versions of software because I like them, even if it puts me at risk of a security vulnerability.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'd rather spend days debugging a microservice than write CSS.",
+        "effect": {
+            "fend": -10,
             "foss": 0,
             "basc": 0,
             "nscl": 0,
@@ -11,5 +143,221 @@ questions = [
             "cent": 0
         }
     },
-    
+    {
+        "question": "Some software should remain closed-source, e.g. mission-critical security.",
+        "effect": {
+            "fend": 0,
+            "foss": -10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "All software must be open-source.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I enjoy editing config files.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I enjoy tuning my desktop environment to my liking.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Gnome > Plasma",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": 'I know what this command does just from reading it:<br><code>for i in *.avi; do ffmpeg -i "$i" "${i%.*}.mp4"; done</code>',
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'd rather use a user-friendly GUI for FFMpeg than manually write commands.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": 10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Older versions of Windows are better.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I've dabbled in newer langauges e.g. Rust, Go",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": 0,
+            "nscl": 10,
+            "conv": 0,
+            "popu": 5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I consider myself very good in coding challenges such as Code Golf.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use FOSS, privacy-friendly alternatives whenever possible.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "It's PHP or nothing.",
+        "effect": {
+            "fend": -10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "The newest JavaScript framework I'm aware of is jQuery.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I agree with the Free Software Foundation's philosophy on software.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "GPL > MIT",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I've dabbled into writing my own esolang, or would like to.<br><span style='font-size: 18px;'><a href='https://en.wikipedia.org/wiki/Esoteric_programming_language' target='_blank'>(what?)</a></span>",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Telemetry is evil, even if anonymous.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": -10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm interested in decentralized backend architecture.",
+        "effect": {
+            "fend": -5,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": -5,
+            "cent": -10
+        }
+    },
+
 ];
