e8fe5cfce4b4c37f5752fd70120a758a8cfbe5be
Axi <98420477+axiand@users.noreply.github.com>
2022-11-21 23:44:13 +0100

    Finish questions (86)

--- questions.js
+++ questions.js
@@ -72,7 +72,7 @@ questions = [
         }
     },
     {
-        "question": "I use Node.js for my app's backend.",
+        "question": "I would rather use Node.js for my app's backend.",
         "effect": {
             "fend": -10,
             "foss": 0,
@@ -84,7 +84,7 @@ questions = [
         }
     },
     {
-        "question": "I use ASP.NET for my app's backend.",
+        "question": "I would rather use ASP.NET for my app's backend.",
         "effect": {
             "fend": -10,
             "foss": 0,
@@ -147,7 +147,7 @@ questions = [
         "question": "Some software should remain closed-source, e.g. mission-critical security.",
         "effect": {
             "fend": 0,
-            "foss": -10,
+            "foss": -5,
             "basc": 0,
             "nscl": 0,
             "conv": 0,
@@ -219,7 +219,7 @@ questions = [
         "question": "I'd rather use a user-friendly GUI for FFMpeg than manually write commands.",
         "effect": {
             "fend": 0,
-            "foss": 5,
+            "foss": 0,
             "basc": 10,
             "nscl": 0,
             "conv": 0,
@@ -359,5 +359,676 @@ questions = [
             "cent": -10
         }
     },
-
+    {
+        "question": "I use decentralized alternatives whenever possible.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": -10
+        }
+    },
+    {
+        "question": "If my messages aren't encrypted with the strongest algorithm, I'm not interested in your messaging app.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "If the backend isn't open-source, I don't trust your service.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Java is amazing.",
+        "effect": {
+            "fend": -10,
+            "foss": -5,
+            "basc": 0,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "MySQL is better than other databases.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I avoid Google like the plague.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I don't like Chromium.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -10,
+            "conv": 5,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "The Web must go back to a time before JavaScript.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "GUIs should be as utilitarian as possible.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I want to have absolute control over the products I buy.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": -5,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I spend most of my time in a command line interface.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Firefox is better.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Matrix > Discord",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -5,
+            "popu": -5,
+            "cent": -10
+        }
+    },
+    {
+        "question": "I still stick to using IRC and E-Mail.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -10,
+            "conv": -5,
+            "popu": -10,
+            "cent": -5
+        }
+    },
+    {
+        "question": "Mastodon is better than other social media apps.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": -5,
+            "cent": -10
+        }
+    },
+    {
+        "question": "I enjoy web design.",
+        "effect": {
+            "fend": 10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I prefer to use forums over social media.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm in my element when I'm optimizing SQL queries.",
+        "effect": {
+            "fend": -10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "The current state of advertising on the Web is horrendous.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": -10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I encrypt my hard drive.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I don't want anyone, not even an automated script or a recommendation algorithm, looking at my Internet activity.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": -5,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm fine with my data being collected to recommend me things I like.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm fine with having usage statistics collected if it helps the developers make the product better.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 5,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Bloat must be minimized at all costs.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use a command-line-based code editor (e.g. Vim) primarily.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -10,
+            "nscl": -5,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "My perfect experience is being given a bare-bones OS and installing everything else exactly the way I like it. (think Arch Linux)",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": -10,
+            "nscl": -5,
+            "conv": -5,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "For simple things, I'd rather write my own functions than install a library someone else made.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm interested in cryptography.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": -5
+        }
+    },
+    {
+        "question": "I'm in my element when I'm writing CSS.",
+        "effect": {
+            "fend": 10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm in my element when I'm optimizing HTML rendering.",
+        "effect": {
+            "fend": 10,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Writing low-level code is my jam.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I love to learn new frameworks and integrate them into my projects.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 10,
+            "conv": 0,
+            "popu": 5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "We mustn't allow corporations to take over the Web.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": 0,
+            "nscl": -5,
+            "conv": -5,
+            "popu": 0,
+            "cent": -5
+        }
+    },
+    {
+        "question": "I tackle complex projects (3D rendering, OSes, etc...) because it brings me joy.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I often submit pull requests to repositories of projects I love.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I practice recreational programming.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I enjoy looking for bugs and submitting issues / security vulnerabilities.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -5,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Sometimes I'll jump into the source code of an app I use and change something to better suit my needs.",
+        "effect": {
+            "fend": 0,
+            "foss": 5,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "It's better to use an established piece of software than whatever's being hyped right now.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I use GUIs whenever possible.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 10,
+            "nscl": 5,
+            "conv": 0,
+            "popu": 5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I feel seriously incapacitated when I have to go into the command line to change system settings, as opposed to using a GUI.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 10,
+            "nscl": 5,
+            "conv": 0,
+            "popu": 5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Networking (as in Internet) confuses me.",
+        "effect": {
+            "fend": 5,
+            "foss": 0,
+            "basc": 10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "The more configurable options, the better.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -10,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm in my element when I'm working with old hardware.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'm very cautious to not leak any info about myself.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 0,
+            "conv": -10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I zealously follow Internet safety precautions.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": 0,
+            "conv": -10,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I have fond memories of working with Flash.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I am proficient in an 'old' programming language (BASIC, COBOL, etc...)",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": -5,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I like trying out niche programming languages.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": 5,
+            "conv": 0,
+            "popu": -10,
+            "cent": 0
+        }
+    },
+    {
+        "question": "The world would be a better place if more software were licensed with the GNU GPL.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": -5,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Copyleft is beneficial to everyone involved.<br><span style='font-size: 18px;'><a href='https://en.wikipedia.org/wiki/Copyleft' target='_blank'>(what?)</a></span>",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I should be able to use software I own in whatever way I wish.",
+        "effect": {
+            "fend": 0,
+            "foss": 10,
+            "basc": 0,
+            "nscl": 0,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I have fond memories of BBSes.<br><span style='font-size: 18px;'><a href='https://en.wikipedia.org/wiki/Bulletin_board_system' target='_blank'>(what?)</a></span>",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": -5,
+            "cent": 0
+        }
+    },
+    {
+        "question": "All or most of the tech in my house is at least a few years old.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "I'd rather use a lightweight but outdated app than a new but bloated one.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
+    {
+        "question": "Old software is oftentimes superior.",
+        "effect": {
+            "fend": 0,
+            "foss": 0,
+            "basc": 0,
+            "nscl": -10,
+            "conv": 0,
+            "popu": 0,
+            "cent": 0
+        }
+    },
 ];
